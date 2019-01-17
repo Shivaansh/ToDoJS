74fb83df3c8c1ecc25e867a5d761c6f7b8f26f53
Shivaansh Prasann <shivaanshseal@gmail.com>
2019-01-17 12:58:38 -0500

    To do delete functionality implemented

--- assets/JS/toDo.js
+++ assets/JS/toDo.js
@@ -1,6 +1,17 @@
 console.log(" toDo.js connected");
 
-//check off ToDos by clicking
+//check off/uncheck ToDos by clicking
 $("li").click(function(){
     $(this).toggleClass("completed"); //class created in CSS file
 });
+
+//click on X to delete TO-DOs
+$("span").click(function(event)
+{
+    //fade out and remove parent <li>
+    $(this).parent().fadeOut("fast", function()
+    {
+        $(this).remove(); //NOT parent
+    });
+    event.stopPropagation(); //prevent event bubbling
+}); 
